528b6dea2520838fd66c39dfdca38d47da308697
derek <derek@turing-drive.com>
2020-11-17 16:01:52 +0800

    modfiy the script

--- openDVR.sh
+++ openDVR.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -i
-dir="."
+run_dir="."
+video_dir=vid_0
+
 nowday="$(date +'%Y_%m_%d')"
 oldday="$(date -d '14 day ago' +'%Y_%m_%d')"
+
 cam_ip=192.168.10.168
+
 
 echo "To day is:" $nowday
 echo "Remove the file of day:" $oldday
 
-# remove 7 days video
+# remove 14 days video
 rm $oldday*.avi
 
 # move to target dir
-cd $dir 
-python3 DVR.py $cam_ip vid_0
+cd $run_dir 
+python3 DVR.py $cam_ip $video_dir
